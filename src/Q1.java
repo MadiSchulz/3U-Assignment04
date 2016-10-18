273f0778463b06eb4c08733edeeb0d096f754b9e
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-10-18 09:39:12 -0400

    10/18. Q1 complete.

---
+++ src/Q1.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author schum0689
+ */
+public class Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Create scanner for input
+        Scanner input = new Scanner(System.in);
+
+        // Ask user for name
+        System.out.println("Please enter your name:");
+        // Get name
+        String name = input.nextLine();
+        // Greet user
+        System.out.println("Hello " + name + ". " + "How are you today?");
+    }
+}
