a8a0b550b02c3b903affc97348689dae94ea20fd
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-10-18 10:14:22 -0400

    10/18. Q2 complete.

--- src/Q2.java
+++ src/Q2.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +16,24 @@ public class Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        // Create scanner for input
+        Scanner input = new Scanner(System.in);
+        
+        // Ask user for inches value
+        System.out.println("Enter the measurement in inches:");
+        
+        // Get inches value from user
+        double inch = input.nextDouble();
+        
+        // Create variable to store answer in cm
+        double centi = inch * 2.54;
+        
+        System.out.println(inch + " inches is the same as " + centi + " cm.");
+        
+
+        
+        
+        
     }
 }
