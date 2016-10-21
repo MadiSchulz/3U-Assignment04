b84a5f56cdb1c53b9b450112eb81999a64ab7459
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-10-21 09:55:05 -0400

    10/21. Working on Q8.

--- src/Q8.java
+++ src/Q8.java
@@ -20,5 +20,41 @@ public class Q8 {
         // Create scanner for input
         Scanner input = new Scanner(System.in);
         
+        // CREATE GAME VARIABLES
+        // Variable to store sum of dice ( >=2 && <=12)
+        int sumDice;
+        // If 0, terminate program
+        // If not >=2 && <=12, do not move
+        // If >=2 && <=12, then move
+
+        // Variable to store first ladder
+        int ladderOne;
+        // 9 to 34, add 25 to squaresMoved
+        // if squaresMoved = 9
+            // int squaresMoved = squaresMoved + 25
+        
+        // Variable to store second ladder
+        int ladderTwo;
+        // 
+        // if squaresMoved = 
+        
+        // Variable to store third ladder
+        int ladderThree;
+        //
+        
+        // Variable to store first snake
+        int snakeOne;
+        
+        // Variable to store second snake
+        int snakeTwo;
+        
+        // Variable to store third snake
+        int snakeThree;
+        
+        // Variable to store total number of squares moved by the player
+        int squaresMoved;
+        // Start game with int squaresMoved = 1;
+        
+        // For every move, add sumDice to squaresMoved
     }
 }
