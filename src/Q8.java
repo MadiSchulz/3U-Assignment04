ed2b6882b093717de75da3eae628f75c49a8d4a8
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-10-28 10:13:47 -0400

    10/28. Working on Q8. Q8, Q10, Q11.

--- src/Q8.java
+++ src/Q8.java
@@ -28,55 +28,60 @@ public class Q8 {
             System.out.println("Enter sum of dice:");
             // Variable to store Player's sum of the dice
             int sumDice = input.nextInt();
-            
+
             // If the dice are not rolled (0)
-            if(sumDice == 0){
+            if (sumDice == 0) {
                 // Output that Player has quit
                 System.out.println("You Quit!");
                 // End loop
                 break;
-                
-            // Else if the sum is between 2 and 12
-            }else if (sumDice <=2 && sumDice >=12){
-                // Add the sum of the dice to the total squares moved to get Player's current square
-                currentSquare = sumDice + currentSquare;
-                
-                // If Player lands on the first ladder (square 9)
-                if (currentSquare == 9) {
-                    // Player climbs up to square 34
-                    currentSquare = 34;
+
+                // Else if the sum is between 2 and 12
+            } else if (sumDice >= 2 && sumDice <= 12) {
+                    // Add the sum of the dice to the total squares moved to get Player's current square
+                    currentSquare = sumDice + currentSquare;
+
+                    // If Player lands on the first ladder (square 9)
+                    if (currentSquare == 9) {
+                        // Player climbs up to square 34
+                        currentSquare = 34;
+                    }
+                    // If Player lands on the second ladder (square 40)
+                    if (currentSquare == 40) {
+                        // Player climbs up to square 65
+                        currentSquare = 65;
+                    }
+                    // If Player lands on the third ladder (square 67)
+                    if (currentSquare == 67) {
+                        // Player climbs up to square 86
+                        currentSquare = 86;
+                    }
+                    // If Player lands on the first snake (square 54)
+                    if (currentSquare == 54) {
+                        // Player slides down to square 19
+                        currentSquare = 19;
+                    }
+                    // If Player lands on the second snake (square 90)
+                    if (currentSquare == 90) {
+                        // Player slides down to square 64
+                        currentSquare = 64;
+                    }
+                    // If Player lands on the third snake (square 99)
+                    if (currentSquare == 99) {
+                        // Player slides down to square 77
+                        currentSquare = 77;
+                    }
+                    // If Player is on or beyond square 100
+                    if (currentSquare >= 100) {
+                        // Output that Player wins
+                        System.out.println("You Win!");
+                    }
+
+                    // Output Player's current square
+                    System.out.println("You are on square " + currentSquare + ".");
+
                 }
-                // If Player lands on the second ladder (square 40)
-                if (currentSquare == 40){
-                    // Player climbs up to square 65
-                    currentSquare = 65;
-                }
-                // If Player lands on the third ladder (square 67)
-                if (currentSquare == 67){
-                    // Player climbs up to square 86
-                    currentSquare = 86;
-                }
-                // If Player lands on the first snake (square 54)
-                if (currentSquare == 54){
-                    // Player slides down to square 19
-                    currentSquare = 19;
-                }
-                // If Player lands on the second snake (square 90)
-                if (currentSquare == 90){
-                    // Player slides down to square 64
-                    currentSquare = 64;
-                }
-                // If Player lands on the third snake (square 99)
-                if (currentSquare == 99){
-                    // Player slides down to square 77
-                    currentSquare = 77;
-                }
-                // If Player is on or beyond square 100
-                if (currentSquare >= 100){
-                    // Output that Player wins
-                    System.out.println("You Win!");
-                }
-            } 
+            }
         }
     }
-}
+
